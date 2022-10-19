d7c62c5ca6b893335fcbf0f362d2a4061e97eaca
jayeshfw230025 <jayeshmasavi2022@gmail.com>
2022-10-19 10:30:54 +0530

    arry

---
+++ arry/index.js
@@ -0,0 +1,7 @@
+//Given an array print the position (starting with 1) and the element in a single line.
+let str="";
+let arr= [1,2,3,4,5,6,7,8,9];
+for (let i=0; i<arr.length; i++){
+  str+=arr[i]+" ";
+} console.log(str);
+
